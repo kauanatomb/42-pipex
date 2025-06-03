12ff57be71a51608a013dd8ae59f885b5839ca68
Kauana Tombolato <kauanatomb@gmail.com>
2025-06-03 10:22:15 +0200

    pipe test

--- main.c
+++ main.c
@@ -17,6 +17,8 @@ int main(int argc, char *argv[], char *envp[])
     (void)envp;
     int infile_fd;
     int outfile_fd;
+    int pipefd[2];
+    int v_pipe;
     if (argc != 5)
         return (ft_printf("Arguments error.\n"), 1);
     infile_fd = open(argv[1], O_RDONLY);
@@ -26,6 +28,10 @@ int main(int argc, char *argv[], char *envp[])
     outfile_fd = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
     if (outfile_fd < 0)
         return (perror("Outfile error"), 1);
-    ft_printf("infile_fd: %d\noutfile_fd: %d\n", infile_fd, outfile_fd);
+    v_pipe = pipe(pipefd);
+    if (v_pipe == -1)
+        return (perror("Pipe error"), 1);
+    ft_printf("pipefd[0] (read end): %d\n", pipefd[0]);
+    ft_printf("pipefd[1] (write end): %d\n", pipefd[1]);
     return (0);
 }
