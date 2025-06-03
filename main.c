44131770cef7081473e4f450b6985cbef29b6699
Kauana Tombolato <kauanatomb@gmail.com>
2025-06-03 10:11:19 +0200

    testing main

---
+++ main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktombola <ktombola@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 09:01:43 by ktombola          #+#    #+#             */
+/*   Updated: 2025/06/03 12:15:28 by ktombola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int main(int argc, char *argv[], char *envp[])
+{
+    (void)envp;
+    int infile_fd;
+    int outfile_fd;
+    if (argc != 5)
+        return (ft_printf("Arguments error.\n"), 1);
+    infile_fd = open(argv[1], O_RDONLY);
+    if (infile_fd < 0)
+        return (perror("Infile error"), 1);
+    
+    outfile_fd = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (outfile_fd < 0)
+        return (perror("Outfile error"), 1);
+    ft_printf("infile_fd: %d\noutfile_fd: %d\n", infile_fd, outfile_fd);
+    return (0);
+}
---
+++ pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktombola <ktombola@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 09:01:43 by ktombola          #+#    #+#             */
+/*   Updated: 2025/06/03 12:15:28 by ktombola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>     // open, O_RDONLY etc.
+# include <sys/types.h>
+# include <sys/stat.h>  // open
+# include <stdio.h>     // perror
+# include "libft/libft.h"
+
+#endif
