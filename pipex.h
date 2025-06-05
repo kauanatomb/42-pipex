cf3bbc6d17611796dbc52276ff7b831ed544f20a
kauanatomb <kauanatomb@gmail.com>
2025-06-05 14:49:16 +0200

    added new lib and new functions

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: ktombola <ktombola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 09:01:43 by ktombola          #+#    #+#             */
-/*   Updated: 2025/06/03 13:01:46 by ktombola         ###   ########.fr       */
+/*   Updated: 2025/06/05 14:48:06 by ktombola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,15 @@
 # include <sys/types.h>
 # include <sys/stat.h>  // open
 # include <stdio.h>     // perror
+# include <sys/wait.h>
 # include "libft.h"
 
 char	*find_command_path(char *cmd, char **envp);
+void	child_process(char **argv, char **envp, int *fd);
+void	second_child_process(char **argv, char **envp, int *fd);
+int		wait_and_check(pid_t pid);
+void	close_fds(int *fds, int count);
+void	free_split(char **paths);
+void	error_exit(const char *msg);
 
 #endif
